f4bf7239b753b8e91bf4a6db499be325ccbb512d
easyboi6969 <104063678+easyboi6969@users.noreply.github.com>
2022-04-20 19:13:29 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400, 400);
+  addition(5, 6)
+}
+
+function draw() {
+  background("aqua");
+
+}
+
+
+
+
+function addition(num1, num2) {
+  var result = num1 + num2
+  console.log(result)
+}
